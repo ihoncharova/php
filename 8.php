fe84aed2bd2bf522f30abef4ee1ec085894eafea
ihoncharova <ihoncharova86@gmail.com>
2017-11-21 17:08:59 +0200

    Some changes

--- 8.php
+++ 8.php
@@ -3,10 +3,11 @@
  * Created by PhpStorm.
  * User: user
  * Date: 21.11.2017
- * Time: 16:14
+ * Time: 16:59
  */
-$age=10;
+$age="fdlk";
 if ($age>18 && $age<59)  echo "Вам еще работать и работать.";
 if ($age>59) echo "Вам пора на пенсию.";
-if ($age<=17) echo "Вам ещё рано работать.";
+if ($age<=17 && $age>0) echo "Вам ещё рано работать.";
+if ($age<=0 or !is_int($age))echo "Неизвестный возраст.";
 ?>
